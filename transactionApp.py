7c0ec098f4c1f291158e49d69bb77cd5979d5264
sonalhanuman <sonalhanuman@gmail.com>
2021-03-12 23:59:14 +0800

    change import flask statement

--- transactionApp.py
+++ transactionApp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import transactionSplitLogic
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from itertools import groupby
 from pprint import pprint
