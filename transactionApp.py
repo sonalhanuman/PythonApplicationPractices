ae0d1dbe5586397786a8fac68d8836881692a414
sonalhanuman <sonalhanuman@gmail.com>
2021-03-12 23:11:40 +0800

    Fix Bugs for Transaction App

--- transactionApp.py
+++ transactionApp.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, request
 import transactionSplitLogic
+from flask_sqlalchemy import SQLAlchemy
+from itertools import groupby
+from pprint import pprint
+from collections import ChainMap
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/countryInfoDatabaseTest'
+db=SQLAlchemy(app)
+
+class DataGroup(db.Model):
+    __tablename__="testdata"
+    id=db.Column(db.Integer, primary_key=True)
+    groupname=db.Column(db.String(120), unique=True)
+    password=db.Column(db.String(20))
+
+    def __init__(self, groupname, password):
+        self.groupname = groupname
+        self.password = password
+
+class DataTrans(db.Model):
+    __tablename__="testtrans"
+    id=db.Column(db.Integer, primary_key=True)
+    groupName=db.Column(db.String(120), unique=False)
+    personName=db.Column(db.String(120), unique=False)
+    moneyAmount=db.Column(db.Float(), nullable=True)
+
+    def __init__(self, personName, moneyAmount, groupName):
+        self.personName = personName
+        self.moneyAmount = moneyAmount
+        self.groupName = groupName
 
 @app.route('/')
 def home():
@@ -12,28 +40,54 @@ def enterMoneyGroup():
     if request.method=='POST':
         groupName=request.form["group_name"]
         password=request.form["password"]
-        transactionSplitLogic.create_transaction_group_table()
-        group = transactionSplitLogic.select_transaction_group(groupName, password)
-        print(group)
-        if len(group)==0:
-            transactionSplitLogic.insert_transaction_group(groupName, password)
-        print(password)
-    return render_template("enterMoneyGroup.html")
+        # transactionSplitLogic.create_transaction_group_table()
+        # group = transactionSplitLogic.select_transaction_group(groupName, password)
+        # print(group)
+        # if len(group)==0:
+        #     transactionSplitLogic.insert_transaction_group(groupName, password)
+        print(groupName, password)
+        if db.session.query(DataGroup).filter(DataGroup.groupname==groupName).count() == 0:
+            data=DataGroup(groupName, password)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("enterMoneyGroup.html", textGroup="You successfully created the group.", group_name=groupName)
+        elif db.session.query(DataGroup).filter(DataGroup.groupname==groupName, DataGroup.password==password).count() == 0:
+            return render_template("transactionSplitFrontEnd.html", textGroup="Your password did not match.")
+        else:
+            return render_template("enterMoneyGroup.html", textGroup="You successfully entered the group.", group_name=groupName)
 
 @app.route("/insertTransactionRecord", methods=['POST'])
 def insertTransactionRecord():
     if request.method=='POST':
         personName=request.form["person_name"]
-        moneyAmount=request.form["money_amount"]
-        groupName = ""
+        moneyAmount=float(request.form["money_amount"])
+        groupName = request.form["group_name"]
+
         password = ""
-        transactionSplitLogic.create_transaction_record_table()
-        transactionSplitLogic.insert_transaction_record(groupName, password, personName, moneyAmount)
-        transactions = transactionSplitLogic.select_transactions_records(personName, moneyAmount)
+        # transactionSplitLogic.create_transaction_record_table()
+        # transactionSplitLogic.insert_transaction_record(groupName, password, personName, moneyAmount)
+        # transactions = transactionSplitLogic.select_transactions_records(personName, moneyAmount)
         # finalMap = transactionSplitLogic.splitMoney(transactions)
-        print(personName)
-        print(moneyAmount)
-    return render_template("enterMoneyGroup.html")
+        data=DataTrans(personName, moneyAmount, groupName)
+        db.session.add(data)
+        db.session.commit()
+        transRecordList = db.session.query(DataTrans).filter(DataTrans.groupName==groupName).all()
+        inputList = []
+        for transRecord in transRecordList:
+            transRecordObj = {
+                'personName': transRecord.personName,
+                'moneyAmount': transRecord.moneyAmount,
+                'groupName': transRecord.groupName
+            }
+            inputList.append(transRecordObj)
+            print(inputList)
+            print(type(inputList))
+        finalResult = transactionSplitLogic.paymentSplitting(inputList)
+        toPrint = ""
+        for dict in finalResult:
+            toPrint = toPrint + str(dict)
+        return render_template("enterMoneyGroup.html", transactionSplitResult=toPrint)
+
 
 if __name__ =="__main__":
     app.run(debug=True)
--- transactionSplitLogic.py
+++ transactionSplitLogic.py
@@ -1,86 +1,35 @@
-import psycopg2
+from itertools import groupby
+from pprint import pprint
+from collections import ChainMap
 
-# def clear_country_table():
-#     conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-#     cur = conn.cursor()
-#     cur.execute("DROP TABLE IF EXISTS")
-#     conn.commit()
-#     conn.close()
+def paymentSplitting(transRecordList):
+    finalList = []
+    filteredList = []
+    accumulateSum=0.00
+    NameList = []
+    filteredList = []
+    for transRecord in transRecordList:
+        print("Hello hi hi" + str(transRecord))
+        NameList.append(transRecord.get("personName"))
+        uniqueNameList = list(set(NameList))
+        print(uniqueNameList)
 
-def create_transaction_group_table():
-    conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS T_TRANSACTIONGROUP (GROUP_NAME TEXT, PASSWORD TEXT)")
-    conn.commit()
-    conn.close()
-def create_transaction_record_table():
-    conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS T_TRANSACTIONRECORD (GROUP_NAME TEXT, PASSWORD TEXT, NAME TEXT, MONEY DECIMAL(9,2))")
-    conn.commit()
-    conn.close()
+    for name in uniqueNameList:
+        personMoney = 0.00
+        for transRecord in transRecordList:
+            if name == transRecord.get("personName"):
+                personMoney = personMoney + transRecord.get("moneyAmount")
+        dict = {"personName": name, "amountPaid": personMoney}
+        filteredList.append(dict)
+    print("this is filtered list hello" + str(filteredList))
+    for transRecord in filteredList:
+        print("this is how much money per payment" + str(transRecord.get("amountPaid")))
+        accumulateSum = accumulateSum + transRecord.get("amountPaid")
+    averagePerPerson = accumulateSum/(len(filteredList))
 
-def insert_transaction_group(group_name, password):
-    conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-    cur = conn.cursor()
-    #cur.execute("INSERT INTO T_COUNTRYPEDIA VALUES ('%s', '%s', '%s')" % (country, capital, population))
-    cur.execute("INSERT INTO T_TRANSACTIONGROUP VALUES (%s, %s)", (group_name, password))
-    conn.commit()
-    conn.close()
+    for transRecord in filteredList:
+        balance = transRecord.get("amountPaid") - averagePerPerson
+        finalList.append({"personName": transRecord.get("personName"), "amountPaid": transRecord.get("amountPaid"), "balance": balance})
 
-def insert_transaction_record(group_name, password, name, money):
-    conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-    cur = conn.cursor()
-    #cur.execute("INSERT INTO T_COUNTRYPEDIA VALUES ('%s', '%s', '%s')" % (country, capital, population))
-    cur.execute("INSERT INTO T_TRANSACTIONRECORD VALUES (%s, %s, %s, %s)", (group_name, password, name, money))
-    conn.commit()
-    conn.close()
-    
-def select_transaction_group(group_name, password):
-    conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM T_TRANSACTIONGROUP WHERE GROUP_NAME=%s AND PASSWORD=%s", (group_name,password))
-    transactionRows=cur.fetchall()
-    conn.close()
-    return transactionRows
-
-def select_transactions_records(name, money):
-    conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM T_TRANSACTIONRECORD WHERE NAME=%s AND MONEY=%s", (name,money))
-    transactionRows=cur.fetchall()
-    conn.close()
-    return transactionRows
-# def view_entire_country_table():
-#     conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-#     cur = conn.cursor()
-#     cur.execute("SELECT * FROM T_COUNTRYPEDIA")
-#     countryrows=cur.fetchall()
-#     conn.close()
-#     return countryrows
-
-# def delete_country_info(country):
-#     conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-#     cur = conn.cursor()
-#     cur.execute("DELETE FROM T_COUNTRYPEDIA WHERE country=%s", (country,))
-#     conn.commit()
-#     conn.close()
-
-# def update_country_info(country, capital, population):
-#     conn = psycopg2.connect("dbname='countryInfoDatabaseTest' user='postgres' password='postgres123' host='localhost' port='5432'")
-#     cur = conn.cursor()
-#     cur.execute("UPDATE T_COUNTRYPEDIA SET capital=%s, population=%s WHERE country=%s", (capital, population, country))
-#     conn.commit()
-#     conn.close()
-
-# Run Sequence
-
-# # clear_country_table()
-# create_country_table()
-# # insert_country_info("Singapore", "Singapore", 5638700)
-# # insert_country_info("Cambodia", "Phnom Penh", 16486542)
-# # insert_country_info("ThailandR", "Phnom Penh", 16486542)
-# # insert_country_info("Peru", "Lima", 32510453)
-# update_country_info("Thailand", "Bangkok", 69625582)
-# # delete_country_info("ThailandR")
-# print(view_entire_country_table())
+    print("the final list ever" + str(finalList))
+    return finalList
